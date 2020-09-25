73999a27b8f5efd9a7d0dd1cf4061f92c2d5b142
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:23:07 +0700

    add

--- test3.php
+++ test3.php
@@ -6,12 +6,12 @@ ini_set('display_errors',1);
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $query =    odbc_exec($conn, " SELECT  invoice_id,Invoice_no,Invoice_date,Due_date,Room_no,Person_id,Person_name,Invoice_amount  FROM invoice  ");
+    $query =    odbc_exec($conn, " SELECT  *  FROM invoice  ");
 
 
     while ($result = odbc_fetch_array($query)) {
 
-          print_r(odbc_fetch_array($query))."<br>";
+            echo $result["invoice_id"]."<br>";
 
     }
  
