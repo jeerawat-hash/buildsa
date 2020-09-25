465ccf2bac8376f8a664e21e61edbc2cc2f4bdce
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:35:19 +0700

    add

--- test3.php
+++ test3.php
@@ -11,7 +11,7 @@ ini_set('display_errors',1);
 
     while ($result = odbc_fetch_array($query)) {
 
-            echo $result["invoice_id"]."<br>";
+            echo $result["Person_name"]."<br>";
 
     }
  
