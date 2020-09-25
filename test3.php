1932f26076dafd525fdbbca72b7844d7e563ae36
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:34:44 +0700

    add

--- test3.php
+++ test3.php
@@ -6,7 +6,7 @@ ini_set('display_errors',1);
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $query =    odbc_exec($conn, " SELECT  *  FROM invoice  ");
+    $query =    odbc_exec($conn, " SELECT  Person_name  FROM invoice  ");
 
 
     while ($result = odbc_fetch_array($query)) {
