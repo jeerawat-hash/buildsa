07e1795df0f98cdd617ccc620fdbef0d19859c2e
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:02:19 +0700

    add

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $q =    odbc_exec($conn, "SELECT top 1 * FROM invoice ");
+    $q =    odbc_exec($conn, "SELECT top 1 * FROM saraya.invoice ");
 
     print_r(odbc_fetch_array($q));
  
