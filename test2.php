7b242239b3ac17d923472125d1c2c50a563f2c39
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:08:15 +0700

    add

--- test2.php
+++ test2.php
@@ -7,20 +7,7 @@ $driver = "MDBTools";
 $dbName = "/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb";
 $db = new PDO("odbc:Driver=$driver;DBQ=$dbName", "", "");
 
-  foreach ($db->query("SELECT 
-Invoice_id,
-Invoice_no,
-Invoice_date,
-Due_date,
-Room_no,
-Person_id,
-Person_name,
-Invoice_amount as 'เดือนนี้',
-Receipt_amount as 'รับเข้า',
-Invoice_pre_amount ,
-Total_invoice as 'ยอดรวมต้องจ่าย', 
- 
-FROM invoice") as $row) {
+  foreach ($db->query("SELECT  invoice_id,Invoice_no,Invoice_date,Due_date,Room_no,Person_id,Person_name,Invoice_amount,Total_invoice,Receipt_amount  FROM invoice") as $row) {
       
       print_r($row)."<br>";
 
