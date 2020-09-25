8a7b7eab3cee93730718b323c3fe6326a499fa26
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 14:54:18 +0700

    add

--- test2.php
+++ test2.php
@@ -4,6 +4,13 @@ if (!($conn = odbc_connect("TestDatabase", "", ""))) {
 echo "Connection Falied.";
 } else {
 echo "Connection Success.";
+
+    $q =    odbc_exec($conn, "SELECT *
+FROM invoice
+WHERE room_no = '111/6'
+ORDER BY invoice_date DESC");
+
+    print_r(odbc_fetch_array($q));
 }
 
 
