02ecac74ae2d2a99213b9a04e0b07a4ca32254da
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:01:41 +0700

    add

--- test2.php
+++ test2.php
@@ -7,7 +7,7 @@ $driver = "MDBTools";
 $dbName = "/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb";
 $db = new PDO("odbc:Driver=$driver;DBQ=$dbName", "", "");
 
-  foreach ($db->query("SELECT  invoice_id,Total_invoice,old_blanace  FROM invoice") as $row) {
+  foreach ($db->query("SELECT  invoice_id,Total_invoice,convert(varchar(10),old_blanace) as old_blanace  FROM invoice") as $row) {
       
       print_r($row)."<br>";
 
