9e551a8cbf801dc222316a7edcf15772c43ccf05
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 17:20:10 +0700

    add

--- test1.php
+++ test1.php
@@ -6,19 +6,7 @@ ini_set('display_startup_errors',1);
 
 $query = 'SELECT * FROM invoice';
 $mdb_file = '/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb';
-$uname = explode(" ",php_uname());
-$os = $uname[0];
-switch ($os){
-  case 'Windows':
-    $driver = '{Microsoft Access Driver (*.mdb)}';
-    break;
-  case 'Linux':
-    echo "Linux\r\n";
-    $driver = 'MDBTools';
-    break;
-  default:
-    exit("Don't know about this OS");
-}
+$driver = 'MDBTools';
 $dataSourceName = "odbc:Driver=$driver;DBQ=$mdb_file;";
 $db = new PDO($dataSourceName);
 $sth = $db->prepare($query);
