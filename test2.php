28e770d6af52029e3df22ef98ed2b5b45f3b2e80
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:26:57 +0700

    add

--- test2.php
+++ test2.php
@@ -4,7 +4,24 @@
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $query =    odbc_exec($conn, "select Person_name from invoice ");
+    $query =    odbc_exec($conn, "SELECT 
+      Invoice_id,
+      Invoice_no,
+      Invoice_date,
+      Due_date,
+      Room_no,
+      Person_id,
+      Person_name,
+      Invoice_amount as 'เดือนนี้',
+      Receipt_amount as 'รับเข้า',
+      Invoice_pre_amount ,
+      Total_invoice as 'ยอดรวมต้องจ่าย',
+      Old_blanace as 'ยอดค้างเดิมรวม'
+       
+      FROM invoice
+      WHERE room_no = '111/6'
+      ORDER BY invoice_date DESC;
+       ");
 
 
     while ($result = odbc_fetch_array($query)) {
