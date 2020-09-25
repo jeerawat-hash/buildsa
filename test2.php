9ed66b046893beb8a1ecb9e97cc7077df24d3a0e
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:41:26 +0700

    add

--- test2.php
+++ test2.php
@@ -5,7 +5,7 @@
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $query =    odbc_exec($conn, "SELECT Old_blanace FROM invoice where Room_no like '111/6%' ");
+    $query =    odbc_exec($conn, "SELECT CONVERT(decimal(28,2), Old_blanace) AS Old_blanace FROM invoice where Room_no like '111/6%' ");
 
 
     while ($result = odbc_fetch_array($query)) {
