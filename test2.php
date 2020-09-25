4baa5b081c9bccd47b5cb66f8e089b21a29ce176
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:32:41 +0700

    add

--- test2.php
+++ test2.php
@@ -5,7 +5,7 @@
  $conn = odbc_connect("TestDatabase", "", "");
 
 
-    $query =    odbc_exec($conn, "SELECT Invoice_id, Invoice_no, Invoice_date,  Due_date, Room_no,  Person_id, Person_name, Invoice_amount , Receipt_amount , Invoice_pre_amount , Total_invoice , Old_blanace FROM invoice");
+    $query =    odbc_exec($conn, "SELECT Invoice_id FROM invoice");
 
 
     while ($result = odbc_fetch_array($query)) {
