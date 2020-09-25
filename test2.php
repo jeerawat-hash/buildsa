0094091d45d1d9b7767f086562fc15e4c8a0f773
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:56:06 +0700

    add

--- test2.php
+++ test2.php
@@ -2,14 +2,17 @@
 <?php 
 ini_set('display_errors',1);
 
+ 
+$driver = "MDBTools";
+$dbName = "/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb";
+$db = new PDO("odbc:Driver=$driver;DBQ=$dbName", "", "");
 
- $conn = odbc_connect("TestDatabase", "", "");
+  foreach ($db->query("SELECT  Old_blanace  FROM invoice") as $row) {
+      
+      print_r($row)."<br>";
 
+}
 
-    $query =    odbc_exec($conn, 'SELECT  Old_blanace  FROM invoice ');
-
-
-  
     
 
 
