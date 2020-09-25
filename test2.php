06b6b488da775e5a422cec641e8dd8b3365dae94
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 16:10:01 +0700

    aadd

--- test2.php
+++ test2.php
@@ -7,7 +7,8 @@ $driver = "MDBTools";
 $dbName = "/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb";
 $db = new PDO("odbc:Driver=$driver;DBQ=$dbName", "", "");
 
-  foreach ($db->query("SELECT  invoice_id,Invoice_no,Invoice_date,Due_date,Room_no,Person_id,Person_name,Invoice_amount,Total_invoice,Receipt_amount  FROM invoice") as $row) {
+  foreach ($db->query("SELECT  invoice_id,Invoice_no,Invoice_date,Due_date,Room_no,Person_id,Person_name,Invoice_amount,Total_invoice,Receipt_amount  FROM invoice WHERE room_no = '111/6%'
+ORDER BY invoice_date DESC") as $row) {
       
       print_r($row)."<br>";
 
