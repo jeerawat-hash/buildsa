a55bcd1b907f84bcef9ceea6ee7da4c3ee6d2263
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-25 15:05:50 +0700

    add

--- test2.php
+++ test2.php
@@ -1,11 +1,17 @@
 <?php 
+/*
  $conn = odbc_connect("TestDatabase", "", "");
 
 
     $q =    odbc_exec($conn, "SELECT top 1 * FROM saraya.invoice ");
 
     print_r(odbc_fetch_array($q));
- 
+ */
+
+    $driver = "MDBTools"; 
+      $dbName = "/home/admin/web/saraya.sakorncable.com/public_html/DB/saraya.mdb"; 
+      $db = new PDO("odbc:Driver=$driver;DBQ=$dbName", "", "");
+
 
 
  ?>
