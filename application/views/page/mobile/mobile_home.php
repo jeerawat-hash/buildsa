96e9fcd173fa7142c7b547d693ef2e92b266ac80
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-12-03 10:42:08 +0700

    add

--- application/views/page/mobile/mobile_home.php
+++ application/views/page/mobile/mobile_home.php
@@ -525,9 +525,9 @@
 
 
         
-        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/mobile_home.js?v=1"></script>
-        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerCheckblanace.js?v=1"></script>
-        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerSendSlip.js?v=1"></script>
+        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/mobile_home.js?v=2"></script>
+        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerCheckblanace.js?v=2"></script>
+        <script src="https://saraya.sakorncable.com/assets/pages/mobile/js/Mobile_CustomerSendSlip.js?v=2"></script>
  
 
 <script>
